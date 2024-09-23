26f096b2959875dad7fa8bc51c2c6615469a5d3d
ideapple0912@gmail.com <ideapple0912@gmail.com>
2024-09-23 11:23:47 +0900

    feat: 일정 엔티티 생성

---
+++ src/main/java/ok/backend/scedule/domain/entity/Routine.java
@@ -0,0 +1,31 @@
+package ok.backend.scedule.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "routine")
+public class Routine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "routine_id")
+    private Long routineId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id", nullable = false)
+    private Schedule schedule;
+
+    @Column(name = "repeat_type", nullable = false)
+    private String repeatType;
+
+    @Column(name = "repeat_interval", nullable = false)
+    private Integer repeatInterval;
+
+    @Column(name = "repeat_on")
+    private String repeatOn;
+}
---
+++ src/main/java/ok/backend/scedule/domain/entity/Schedule.java
@@ -0,0 +1,62 @@
+package ok.backend.scedule.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import ok.backend.member.domain.Member;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Table(name = "schedules")
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "schedule_id")
+    private Long scheduleId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Member user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @CreationTimestamp
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private LocalDateTime createAt;
+
+    @UpdateTimestamp
+    @Column(name = "update_at", nullable = false)
+    private LocalDateTime updateAt;
+
+    @Column
+    private Integer importance;
+
+    @OneToOne(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Routine routine;
+
+    @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TeamSchedule> teamSchedules = new ArrayList<>();
+}
