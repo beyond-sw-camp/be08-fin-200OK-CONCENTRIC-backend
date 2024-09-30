fc89a7a5437eb11592bcc288067636d6bbb05a9e
byHyen <chi0666@naver.com>
2024-09-30 15:02:05 +0900

    teamListRepository 수정

--- src/main/java/ok/backend/team/domain/repository/TeamListRepository.java
+++ src/main/java/ok/backend/team/domain/repository/TeamListRepository.java
@@ -5,12 +5,11 @@ import ok.backend.team.domain.entity.TeamList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TeamListRepository extends JpaRepository<TeamList, Long> {
     List<TeamList> findByMemberId(Long memberId);
 
     boolean existsByTeamIdAndMemberId(Long teamId, Long memberId);
 
-    Optional<TeamList> findByTeamId(Long teamId);
+    List<TeamList> findByTeamId(Long teamId);
 }
