a9442267934e3fe43edd6f3eabd25ac443ac0c09
박성준 <macleod.park@gmail.com>
2024-10-03 16:03:56 +0900

    feat: 파일 다운로드 테스트

--- src/main/java/ok/backend/storage/controller/StorageController.java
+++ src/main/java/ok/backend/storage/controller/StorageController.java
@@ -6,11 +6,16 @@ import lombok.RequiredArgsConstructor;
 import ok.backend.storage.domain.enums.StorageType;
 import ok.backend.storage.dto.StorageUploadResponseDto;
 import ok.backend.storage.service.StorageService;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
 
 @Tag(name = "Storage", description = "파일 전송/저장")
@@ -29,6 +34,17 @@ public class StorageController {
         List<StorageUploadResponseDto> storageUploadResponseDtos =  storageService.uploadFileToStorage(ownerId, storageType, files);
 
         return ResponseEntity.ok(storageUploadResponseDtos);
+    }
+
+    @Operation(summary = "파일 다운로드 API")
+    @PostMapping(value = "/download")
+    public ResponseEntity<Resource> download() throws MalformedURLException {
+        UrlResource resource = new UrlResource("file:" + "/Users/sjpark/Desktop/Storage/profiles/1_profile_image.jpeg");
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
