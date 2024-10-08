c670badb87e70019b71878b39f75d24866c1519e
박성준 <macleod.park@gmail.com>
2024-10-08 15:41:45 +0900

    refactor: schedule 조회 시 routine, team_schedule fetch join 하도록 변경

--- src/main/java/ok/backend/schedule/domain/repository/ScheduleRepository.java
+++ src/main/java/ok/backend/schedule/domain/repository/ScheduleRepository.java
@@ -1,10 +1,12 @@
 package ok.backend.schedule.domain.repository;
 
 import ok.backend.schedule.domain.entity.Schedule;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    @EntityGraph(attributePaths = {"routine", "teamSchedule"})
     List<Schedule> findByMemberId(Long memberId);
 }
