8cb9da49c5e4e01e6bc3f2bdd3eb48f0123f542e
박성준 <macleod.park@gmail.com>
2024-10-24 21:24:51 +0900

    refactor: 팀 dto 수정, 로직 수정

--- src/main/java/ok/backend/team/dto/TeamResponseDto.java
+++ src/main/java/ok/backend/team/dto/TeamResponseDto.java
@@ -1,19 +1,15 @@
 package ok.backend.team.dto;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import ok.backend.team.domain.entity.Team;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class TeamResponseDto {
     private Long id;
-
-//    private Long chatroom_id;
 
     private String name;
 
@@ -24,14 +20,11 @@ public class TeamResponseDto {
     private String imageUrl;
 
     public TeamResponseDto(Team team) {
-
         this.id = team.getId();
-        //this.chatroom_id = team.getChatroom().getId();
         this.name = team.getName();
         this.creatorId = team.getCreatorId();
         this.createAt = team.getCreateAt();
         this.imageUrl = team.getImageUrl();
 
     }
-
 }
