88e064a4e256417e1690c03785d6fdf710e2f594
박성준 <macleod.park@gmail.com>
2024-10-24 18:24:02 +0900

    refactor: 팀 dto 수정

--- src/main/java/ok/backend/team/dto/TeamRequestDto.java
+++ src/main/java/ok/backend/team/dto/TeamRequestDto.java
@@ -1,16 +1,13 @@
 package ok.backend.team.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
+import lombok.*;
 
 
 @Getter
 @Setter
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TeamRequestDto {
 
     private String name;
