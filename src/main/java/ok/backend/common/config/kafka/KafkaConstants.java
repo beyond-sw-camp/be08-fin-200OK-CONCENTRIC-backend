6e3295d83cdce923d5c32195023ed7cf92eeadbd
henhen7 <eatho@naver.com>
2024-11-06 00:26:39 +0900

    refactor: wss 엔드포인트 변경

--- src/main/java/ok/backend/common/config/kafka/KafkaConstants.java
+++ src/main/java/ok/backend/common/config/kafka/KafkaConstants.java
@@ -5,5 +5,4 @@ import java.util.UUID;
 public class KafkaConstants {
 //    public static String name = UUID.randomUUID().toString();
     public static final String GROUP_ID = "chat-group";
-    public static final String BROKER = "localhost:9092";
 }
